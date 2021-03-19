2fcde27ea1ae55bd3063cd59669d4f149d42a87f
Ugo Cottin <ugo.cottin@gmail.com>
2021-03-20 00:51:13 +0100

    initial commit

---
+++ Sources/FluentFirebirdDriver/FluentFirebirdConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ugo Cottin on 15/03/2021.
+//
+
+import Foundation
---
+++ Sources/FluentFirebirdDriver/back/SQL/FirebirdSQLDatabase.swift
@@ -0,0 +1,12 @@
+//
+//  FirebirdSQLDatabase.swift
+//  
+//
+//  Created by Ugo Cottin on 16/03/2021.
+//
+
+struct FirebirdSQLDatabase {
+	let database: FirebirdDatabase
+	let encoder: FirebirdDataEncoder
+	let decoder: FirebirdDataDecoder
+}
